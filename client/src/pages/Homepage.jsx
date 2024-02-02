d5bfb7fb1e99d04b381eb7d67e9f4516054cf04f
mihirb1234 <67053200+mihirb1234@users.noreply.github.com>
2024-02-03 00:38:10 +0530

    2

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Homepage = () => {
-  return (
-    <div>Hahahahaa</div>
-  )
-}
+  return <div>this is my homepage</div>;
+};
 
-export default Homepage
+export default Homepage;
