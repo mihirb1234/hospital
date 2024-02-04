aa052d5fa49c71798751187881759fd7fa7aac4c
mihirb1234 <67053200+mihirb1234@users.noreply.github.com>
2024-02-04 19:56:47 +0530

    register 2

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import Link from "antd/es/typography/Link";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <h1 className="text-black bg-fuchsia-300">heihefho</h1>;
+  // const handleSubmit=()=>{}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <div className="h-screen py-20 bg-gradient-to-r from-fuchsia-200 to-indigo-500">
+        <div className="container mx-auto my-20 ">
+          <div className="w-2/3 mt-10   bg-white rounded-xl mx-auto shadow-lg overflow-hidden flex justify-center items-center">
+            <div className="bg-white">
+              <h2 className="text-3xl text-center mt-10">Login</h2>
+              <p className="mt-4 text-center">Enter your details here</p>
+              <form action="#" onSubmit={handleSubmit}>
+                <div className="p-3 flex flex-col gap-3 mt-3 justify-center items-center mb-10  ">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                    onChange={handleChange}
+                    value={formData.email}
+                    className="w-full  border-2 border-blue-700 rounded-lg px-2 py-2"
+                  />
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                    onChange={handleChange}
+                    value={formData.password}
+                    className="w-full border-gray-900 border-2 rounded-lg px-2 py-2 "
+                  />
+                  <Link>Dont have an account? Create Account</Link>
+                  <button
+                    type="submit"
+                    className="bg-blue-400 p-3 text-white font-bold rounded-xl shadow-lg px-5 mt-3 transition-transform transform hover:scale-105"
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { message } from "antd";
 import axios from "axios";
+
 const Register = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
@@ -9,6 +10,7 @@ const Register = () => {
     email: "",
     password: "",
   });
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -23,7 +25,7 @@ const Register = () => {
     try {
       const res = await axios.post("/api/v1/user/register", formData);
       if (res.data.success) {
-        message.success("registered succesfully");
+        message.success("registered successfully");
         navigate("/login");
       } else {
         message.error(res.data.message);
@@ -37,22 +39,23 @@ const Register = () => {
   return (
     <>
       <div className="min-h-screen py-20 bg-gradient-to-r from-fuchsia-200 to-indigo-500">
-        <div className="container mx-auto   ">
-          <div className="w-2/3 bg-white rounded-xl mx-auto shadow-lg overflow-hidden flex">
+        <div className="container mx-auto mt-20">
+          <div className="lg:w-1/2 bg-white rounded-xl mx-auto shadow-lg overflow-hidden flex">
             <div className="w-1/2">
               <img
-                src="https://images.unsplash.com/photo-1530026405186-ed1f139313f8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src="https://plus.unsplash.com/premium_photo-1676325101840-a324d28fd19b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="heart"
+                className="object-cover h-full w-full"
               />
             </div>
             <div className="w-1/2 bg-pink-300">
-              <h2 className="text-center mt-5 font-extrabold text-white text-2xl">
+              <h2 className="text-center mt-5 font-extrabold text-white lg:text-6xl text-2xl">
                 Register
               </h2>
-              <p className="text-xs text-center mt-2">Create your account.</p>
+              <p className="text-xs text-center mt-10">Create your account.</p>
 
               <form action="#" onSubmit={handleSubmit}>
-                <div className="p-3">
+                <div className="sm:p-2 lg:p-5">
                   <input
                     type="text"
                     name="name"
@@ -60,7 +63,7 @@ const Register = () => {
                     required={true}
                     onChange={handleChange}
                     value={formData.name}
-                    className="w-full border-gray-500 rounded-lg mt-4 px-2 py-2"
+                    className="w-full border-gray-500 rounded-lg mt-4 px-2 py-2 sm:py-1 md:py-2"
                   />
                   <input
                     type="email"
@@ -69,7 +72,7 @@ const Register = () => {
                     onChange={handleChange}
                     required={true}
                     value={formData.email}
-                    className="w-full border-gray-500 rounded-lg mt-2 px-2 py-2"
+                    className="w-full border-gray-500 rounded-lg mt-2 px-2 py-2 sm:py-1 md:py-2"
                   />
                   <input
                     type="password"
@@ -78,14 +81,16 @@ const Register = () => {
                     required={true}
                     value={formData.password}
                     onChange={handleChange}
-                    className="w-full border-gray-500 rounded-lg mt-2 px-2 py-2"
+                    className="w-full border-gray-500 rounded-lg mt-2 px-2 sm:py-1 py-2 md:py-2"
                   />
                   <div className="py-2">
-                    <Link to="/login">Already registered? Login</Link>
+                    <Link to="/login" className="sm:text-lg text-xs">
+                      Already registered? Login
+                    </Link>
                   </div>
                   <button
                     type="submit"
-                    className="bg-blue-400 p-3 text-white font-bold rounded-xl shadow-lg px-5 mt-10 transition-transform transform hover:scale-105"
+                    className="bg-blue-400 p-3 text-white ml-1 mb-3 font-bold rounded-xl shadow-lg lg:px-5 mt-10 transition-transform transform hover:scale-105"
                   >
                     REGISTER
                   </button>
@@ -98,4 +103,5 @@ const Register = () => {
     </>
   );
 };
+
 export default Register;
